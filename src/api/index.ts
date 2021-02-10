d65f406e64cce1b3c2c011346cf3bdd85084cd0e
Edgar <edgdav00@gmail.com>
2021-02-10 14:26:10 -0600

    inicio Proyecto

---
+++ src/api/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+// import auth from './auth/auth.routes';
+// import products from './products/product.routes';
+// import authMiddleware from './middleware/auth.middleware';
+
+const router: Router = Router();
+
+// router.use('/', auth);
+// router.use('/products', authMiddleware, products);
+export default router;
