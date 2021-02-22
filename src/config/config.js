a327dac020faa36aa6847b6cb7ab5705ab4755bb
Edgar <edgdav00@gmail.com>
2021-02-21 22:17:00 -0600

    fixes for build

--- src/config/config.js
+++
@@ -1,8 +0,0 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-export default {
-    PORT: process.env.PORT,
-    EMAIL: process.env.EMAIL,
-    EMAIL_PASS: process.env.EMAIL_PASS,
-    EMAIL_RECEIVER: process.env.EMAIL_RECEIVER,
-};
